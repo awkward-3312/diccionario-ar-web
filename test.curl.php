bb85f9bcd72d2c61a7b8054cc287a326adc951e0
awkward-3312 <betza_es77@hotmail.com>
2025-05-22 15:45:14 -0600

    Create test.curl.php

---
+++ test.curl.php
@@ -0,0 +1,14 @@
+<?php
+header('Content-Type: text/plain');
+
+$ch = curl_init("https://translate.astian.org/translate");
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+$response = curl_exec($ch);
+$error = curl_error($ch);
+$code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+curl_close($ch);
+
+echo "Código de respuesta: $code\n";
+echo "Error de cURL: $error\n";
+echo "Respuesta:\n$response";
+?>
