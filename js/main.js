e6e2dca82e7df46aa304d501811a519cebddc95e
awkward-3312 <betza_es77@hotmail.com>
2025-05-26 02:30:56 -0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -218,3 +218,8 @@ if ('serviceWorker' in navigator) {
       .catch(err => console.error('❌ Error SW:', err));
   });
 }
+
+// Exponer funciones globales si se usa onclick en HTML
+window.buscar = buscar;
+window.limpiarBusqueda = limpiarBusqueda;
+window.actualizarGlosario = actualizarGlosario;
