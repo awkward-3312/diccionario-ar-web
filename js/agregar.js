746c1b80f611cedd35a6411cbbec5baa9e93f9c8
awkward-3312 <betza_es77@hotmail.com>
2025-05-30 09:24:30 -0600

    Update agregar.js

--- js/agregar.js
+++ js/agregar.js
@@ -5,7 +5,7 @@ window.addEventListener('DOMContentLoaded', () => {
   }
 
   const supabaseUrl = 'https://gapivzjnehrkbbnjtvam.supabase.co';
-  const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9...'; // tu key
+  const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdhcGl2empuZWhya2Jibmp0dmFtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDg0NjkwMzYsImV4cCI6MjA2NDA0NTAzNn0.g7MXXPDzBqssewgHUreA_jNbRl7A_gTvaTv2xXEwHTk';
   const client = window.supabase.createClient(supabaseUrl, supabaseKey);
 
   function mostrarCampos() {
