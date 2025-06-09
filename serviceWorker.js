39c6c8e771f4139e5903dfe21398eb29bb4a9b59
awkward-3312 <betza_es77@hotmail.com>
2025-06-08 20:46:40 -0600

    Use Git LFS for binary assets

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,59 +1,57 @@
-const CACHE_NAME = "diccionario-ar-cache-v3";
+const CACHE_NAME = 'diccionario-ar-v4';
 
-const urlsToCache = [
-  "/",
-  "/index.html",
-  "/admin.html",
-  "/agregar.html",
-  "/sugerencias.html",
-  "/revisar-sugerencias.html",
-  "/traductor.html",
-  "/favicon-ar.png",
-  "/favicon-ar.ico",
-  "/fonts/Middle-of-April.ttf",
-  "/img/FONDOOO.jpeg",
-  "/img/fondo-claro.png",
-  "/css/estilos.css",
-  "/css/admin.css",
-  "/css/agregar.css",
-  "/css/sugerencias.css",
-  "/css/revisar-sugerencias.css",
-  "/css/traductor.css",
-  "/js/main.js",
-  "/js/admin.js",
-  "/js/agregar.js",
-  "/js/sugerencias.js",
-  "/js/revisar-sugerencias.js",
-  "/js/traductor.js"
+const STATIC_ASSETS = [
+  '/',
+  '/index.html',
+  '/inicio.html',
+  '/admin.html',
+  '/agregar.html',
+  '/sugerencias.html',
+  '/revisar-sugerencias.html',
+  '/chat-sparkie.html',
+  '/css/estilos.css',
+  '/css/inicio.css',
+  '/css/admin.css',
+  '/css/agregar.css',
+  '/css/sugerencias.css',
+  '/css/revisar-sugerencias.css',
+  '/css/chat-sparkie.css',
+  '/js/main.js',
+  '/js/inicio.js',
+  '/js/admin.js',
+  '/js/agregar.js',
+  '/js/sugerencias.js',
+  '/js/revisar-sugerencias.js',
+  '/js/chat-sparkie.js',
+  '/img/sparkie-icon.png',
+  '/favicon-ar.png'
 ];
 
-self.addEventListener("install", (event) => {
-  self.skipWaiting(); // ✅ activa de inmediato
+self.addEventListener('install', event => {
+  self.skipWaiting();
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(urlsToCache);
-    })
+    caches.open(CACHE_NAME).then(cache => cache.addAll(STATIC_ASSETS))
   );
 });
 
-self.addEventListener("activate", (event) => {
+self.addEventListener('activate', event => {
   event.waitUntil(
-    caches.keys().then((cacheNames) =>
-      Promise.all(
-        cacheNames.map((name) => {
-          if (name !== CACHE_NAME) {
-            return caches.delete(name);
-          }
-        })
-      )
+    caches.keys().then(keys =>
+      Promise.all(keys.map(key => key !== CACHE_NAME && caches.delete(key)))
     )
   );
+  self.clients.claim();
 });
 
-self.addEventListener("fetch", (event) => {
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
   event.respondWith(
     fetch(event.request)
-      .then((response) => response)
+      .then(res => {
+        const resClone = res.clone();
+        caches.open(CACHE_NAME).then(cache => cache.put(event.request, resClone));
+        return res;
+      })
       .catch(() => caches.match(event.request))
   );
 });
