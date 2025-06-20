5d45c1048aea139c4e1a91a7c66e6b818a26080e
awkward-3312 <betza_es77@hotmail.com>
2025-06-20 09:06:31 -0600

    Update dashboard.js

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,7 +1,6 @@
 document.addEventListener('DOMContentLoaded', async () => {
-  const supabaseUrl = 'https://gapivzjnehrkbbnjtvam.supabase.co';
-  const supabaseKey =
-    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdhcGl2empuZWhya2Jibmp0dmFtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDg0NjkwMzYsImV4cCI6MjA2NDA0NTAzNn0.g7MXXPDzBqssewgHUreA_jNbRl7A_gTvaTv2xXEwHTk';
+const supabaseUrl = 'https://gapivzjnehrkbbnjtvam.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdhcGl2empuZWhya2Jibmp0dmFtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDg0NjkwMzYsImV4cCI6MjA2NDA0NTAzNn0.g7MXXPDzBqssewgHUreA_jNbRl7A_gTvaTv2xXEwHTk';
   const supabase = window.supabase.createClient(supabaseUrl, supabaseKey);
   const loader = document.getElementById('loader');
 
